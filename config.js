a18834fd81ccb57d75c87def1239592b0bb1838a
orbit6969 <75386055+orbit6969@users.noreply.github.com>
2020-12-02 23:25:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   prefix: '_',
-  token: 'NzgyOTQxNDU2OTk4MjAzMzky.X8ThDQ.z_gkUvHYmi75rGBM-e3x22AyTqE',
+  token: 'NzgyOTQxNDU2OTk4MjAzMzky.X8ThDQ.kW5PMPPgv4_A7wE6tC0TZCKy6_Q',
   roleId: '783744477755736114',
   roleName: 'Bot.ORB',
   devId: '572327928646598667',
